208d74d6deaa56b04a72b3711812359ff8f487fe
Jack Herrington <jherr@pobox.com>
2024-07-01 18:04:56 -0700

    Test query

---
+++ load-embeddings/test-query.mjs
@@ -0,0 +1,19 @@
+import ollama from "ollama";
+import postgres from "postgres";
+
+const sql = postgres({});
+
+const response = await ollama.embeddings({
+  model: "snowflake-arctic-embed",
+  prompt: "food",
+});
+
+const query = await sql`SELECT position, text FROM
+lines
+ORDER BY
+embedding <#> ${`[${response.embedding.join(",")}]`} 
+LIMIT 10`;
+
+console.log(query.map((row) => `${row.position} ${row.text}`).join("\n"));
+
+sql.end();
